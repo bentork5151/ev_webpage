7c592770fba37452885c352c199eb76c1e4373e6
VedantDhoble <vdhoble80@gmail.com>
2025-12-12 15:26:15 +0530

    Project flow minor Improvements

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -7,8 +7,6 @@ import ApiService from '../services/api.service'
 import API_CONFIG from '../config/api.config'
 import APP_CONFIG from '../config/app.config'
 import { useAuth } from '../store/AuthContext'
-import ThemedImage from '../context/ThemedImage'
-import { ASSETS } from '../assets/images/images.config'
 
 import "../assets/styles/SplashScreen.css"; // ✅ Correct CSS path
 import "@material/web/progress/linear-progress.js"; // ✅ Material Web progress
@@ -61,6 +59,7 @@ const SplashScreen = () => {
           const chargerResponse = await ApiService.get(
             API_CONFIG.ENDPOINTS.GET_CHARGER(ocppId)
           )
+          console.info('Charger Data:', chargerResponse)
           updateChargerData(chargerResponse)
         } catch(error){
           console.error('Failed to load charger data:', error)
@@ -75,7 +74,7 @@ const SplashScreen = () => {
       setStatus('Checking authentication...')
       // const authResult = await AuthService.verifyCachedCredentials()
       // setProgress(80)
-      setStatus('Finalizing...')
+      setStatus('Securing Communication...')
 
       await new Promise(resolve => setTimeout(resolve, APP_CONFIG.UI.SPLASH_DURATION))
       // setProgress(100)
@@ -95,8 +94,8 @@ const SplashScreen = () => {
     <Box className="splash-container">
       <img src={"https://github.com/bentork5151/assets/blob/main/Logo/logo_transparent.png?raw=true"} alt="Bentork Logo" className="splash-logo" />
       <md-linear-progress indeterminate></md-linear-progress>
-      {/* <Bar variant="determinate" value={progress} />
-      <Status>{status}</Status> */}
+      {/* <Bar variant="determinate" value={progress} /> */}
+      <p>{status}</p>
     </Box>
   )
 }
